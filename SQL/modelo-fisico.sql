5fd8e8ffe3c10d67bf7b4ac79bcc8233dda15dfc
Marcelo Bicudo <marcelobicudo.silva@gmail.com>
2022-06-04 17:37:23 -0300

    criando modelo físico da base de dados

---
+++ SQL/modelo-fisico.sql
@@ -0,0 +1,43 @@
+CREATE TABLE regioes (
+    id_regiao NUMBER NOT NULL AUTO_INCREMENT,
+    nm_regiao VARCHAR NOT NULL,
+    CONSTRAINT regioes_pk PRIMARY KEY(id_regiao)
+);
+
+CREATE TABLE doencas (
+    id_doenca NUMBER NOT NULL AUTO_INCREMENT,
+    nm_doenca VARCHAR NOT NULL,
+    CONSTRAINT doencas_pk PRIMARY KEY(id_doenca)
+);
+
+CREATE TABLE zonas (
+    id_zona NUMBER NOT NULL AUTO_INCREMENT,
+    nm_zona VARCHAR NOT NULL,
+    CONSTRAINT zonas_pk PRIMARY KEY(id_zona)
+);
+
+CREATE TABLE faixas_etarias (
+    id_faixa_etaria NUMBER NOT NULL AUTO_INCREMENT,
+    vl_idade_inicial NUMBER NOT NULL,
+    vl_idade_final NUMBER NOT NULL,
+    CONSTRAINT faixas_etarias_pk PRIMARY KEY(id_faixa_etaria)
+);
+
+CREATE TABLE dados_monitoramento (
+    id_monitoramento NUMBER NOT NULL AUTO_INCREMENT,
+    id_regiao NUMBER NOT NULL,
+    id_doenca NUMBER NOT NULL,
+    id_faixa_etaria NUMBER NOT NULL,
+    id_zona NUMBER NOT NULL,
+    dt_registro DATE NOT NULL,
+    ds_sexo CHAR(1) NOT NULL,
+    is_gestante BOOLEAN NOT NULL,
+    nr_casos_confirmados NUMBER NOT NULL,
+    nr_casos_suspeitos NUMBER NOT NULL,
+    nr_obitos NUMBER NOT NULL,
+    CONSTRAINT dados_monitoramento_pk PRIMARY KEY(id_monitoramento),
+    CONSTRAINT dados_monitoramento_regioes_fk FOREIGN KEY(id_regiao) REFERENCES regioes(id_regiao),
+    CONSTRAINT dados_monitoramento_doencas_fk FOREIGN KEY(id_doenca) REFERENCES doencas(id_doenca),
+    CONSTRAINT dados_monitoramento_faixas_etarias_fk FOREIGN KEY(id_faixa_etaria) REFERENCES faixas_etarias(id_faixa_etaria),
+    CONSTRAINT dados_monitoramento_zonas_fk FOREIGN KEY(id_zona) REFERENCES zonas(id_zona)
+);

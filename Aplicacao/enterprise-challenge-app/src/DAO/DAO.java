57f78d4513f9c8f763e6de811ab1da248450233a
marcelobicudo <marcelobicudo.silva@gmail.com>
2022-06-04 20:28:46 -0300

    criando classe DAO de conexão com o banco

---
+++ Aplicacao/enterprise-challenge-app/src/DAO/DAO.java
@@ -0,0 +1,70 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DAO {
+    private Connection conn;
+
+    public DAO () {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        openConnection();
+        return this.conn;
+    }
+    private void openConnection() {
+        try {
+            if(this.conn == null) {
+                this.conn = DriverManager.getConnection("jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", "RM92031", "170284");
+            }
+            System.out.println("Conexão está aberta");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        try {
+            if(this.conn != null) {
+
+                this.conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public int executeCommand(PreparedStatement stmt) {
+        int j = 0;
+        try {
+            j = stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            closeConnection();
+        }
+        return j;
+    }
+
+    public ResultSet getData(PreparedStatement stmt) {
+        ResultSet rs = null;
+        try {
+            rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+}
